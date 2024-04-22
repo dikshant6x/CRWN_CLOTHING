6a1a626122bdc87c6985bdcd09f14443b01c19f2
Dikshant <dikshant@Dikshants-MacBook-Pro.local>
2024-04-22 10:47:46 +0530

    Using Reducers to optimize code for better usability

--- src/components/contexts/cart.context.jsx
+++ src/components/contexts/cart.context.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
 import { onAuthStateChangedListener } from "../../utils/firebase/firebase.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 // adding items to cart
 
@@ -55,41 +56,107 @@ export const CartContext = createContext({
   cartTotal: 0,
 });
 
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const CART_ACTION_TYPES = {
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false); // for icon dropdown
-  const [cartItems, SetCartItems] = useState([]); // for cart items
-  const [cartCount, setCartCount] = useState(0); // for total cart items count to show in icon
-  const [cartTotal, setCartTotal] = useState(0); // for total cart items count to show in icon
+  // const [isCartOpen, setIsCartOpen] = useState(false);         // for icon dropdown
+  // const [cartItems, SetCartItems] = useState([]);             // for cart items
+  // const [cartCount, setCartCount] = useState(0);                // for total cart items count to show in icon
+  // const [cartTotal, setCartTotal] = useState(0);              // for total cart items count to show in icon
 
-  useEffect(() => {
-    const newcartCount = cartItems.reduce(
-      (total, cartItem) => total + cartItem.quantity,
-      0
-    ); // reduce takes 2 aregumenst ist callback , second is starting value
-    setCartCount(newcartCount);
-  }, [cartItems]); // this whole useEffect gets us total no.of  items in cart
+  const [{ cartItems, isCartOpen, cartCount, cartTotal }, dispatch] =
+    useReducer(cartReducer, INITIAL_STATE);
 
-  // To calculate Total price of commodities
-  useEffect(() => {
-    const newCartTotal = cartItems.reduce(
+  // useEffect(() => {
+  //   const newcartCount = cartItems.reduce(
+  //     (total, cartItem) => total + cartItem.quantity,
+  //     0
+  //   );                          // reduce takes 2 aregumenst ist callback , second is starting value
+  //   setCartCount(newcartCount);
+  // }, [cartItems]);             // this whole useEffect gets us total no.of  items in cart
+
+  // // To calculate Total price of commodities
+  // useEffect(() => {
+  //   const newCartTotal = cartItems.reduce(
+  //     (total, cartItem) => total + cartItem.quantity * cartItem.price,
+  //     0
+  //   );                  // reduce takes 2 aregumenst ist callback , second is starting value
+  //   setCartTotal(newCartTotal);
+  // }, [cartItems]);                   // this whole useEffect gets us total no.of  items in cart
+
+  // REDUCER SPECIFIC FUNCTION USED TO UPDAET
+
+  const updateCartItemsReducer = (newCartItems) => {
+    //generate newCartTotal
+
+    const newCartTotal = newCartItems.reduce(
       (total, cartItem) => total + cartItem.quantity * cartItem.price,
       0
-    ); // reduce takes 2 aregumenst ist callback , second is starting value
-    setCartTotal(newCartTotal);
-  }, [cartItems]); // this whole useEffect gets us total no.of  items in cart
+    );
+
+    //generate newCartCount
+
+    const newcartCount = newCartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartTotal: newCartTotal,
+        cartCount: newcartCount,
+      })
+    );
+  };
 
   const addItemToCart = (productToAdd) => {
-    SetCartItems(addCartItem(cartItems, productToAdd));
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartItemsReducer(newCartItems);
   }; // function that triggers whenever user click on any product depending on availablity of product in cart item it either increase quantity added or it simply adds product to the cart item
   const removeItemToCart = (cartItemToRemove) => {
-    SetCartItems(removeCartItem(cartItems, cartItemToRemove));
+    const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+    updateCartItemsReducer(newCartItems);
   };
 
   // function to clear a specifi product on clcik to remove the product in checkout page
   const clearItemFromCart = (cartItemToClear) => {
-    SetCartItems(clearCartItem(cartItems, cartItemToClear));
+    const newCartItems = clearCartItem(cartItems, cartItemToClear);
+    updateCartItemsReducer(newCartItems);
   };
 
+  // functiomn to dispatch for setIsCartOpen
+  const setIsCartOpen = (bool) => {
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool));
+  };
   const value = {
     isCartOpen,
     setIsCartOpen,
@@ -102,3 +169,7 @@ export const CartProvider = ({ children }) => {
   };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
+
+// REASON TO USE REDUCERS IN CONTEXT HERE I SSO WE CAN CONGREGATE SOME LOGICS TO DISPATCH UPDATS FOR MULTIPLE ATTRIBUTES IN A SINGLE DISPATCH
+
+// BASICALLY MODIFY MULTIPLE READABLE VALUES INSIDE STATE
--- src/components/contexts/user.context.jsx
+++ src/components/contexts/user.context.jsx
@@ -1,20 +1,54 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   createUserDocumentFromAuth,
   onAuthStateChangedListener,
 } from "../../utils/firebase/firebase.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 // export out context as userContext
 // as the actual value you want to access
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
-}); // Here we pass value to createContext object with values null and Setter function wth null op/
+});
+// Here we pass value to createContext object with values null and Setter function wth null o/p
 // Passing in default value in create context when initially creating it
 // Current value of state
 
+// ADDING REDUCER
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log("dispatched");
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null); // currentUser = value of useState ; setCurrentUser = setter function of useState
+  // const [currentUser, setCurrentUser] = useState(null); // currentUser = value of useState ; setCurrentUser = setter function of useState
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE); // utilizing usereducer hook we have 2 inpit attr usereducer= reducerfuction ; INITIAL STATE = initial value for state  ;  what we are extracting is State Object and dispatch function
+  // const { currentUser } = state; you cna see above we added it direct ly as {currentUser} rather than
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user)); // here these are attr of action obj that goes to userReducer and update our o/p
+  };
   const value = { currentUser, setCurrentUser };
 
   // Here we consolidtae our whole codefor listner callback to listen forany auth stet change that now is place under user context
